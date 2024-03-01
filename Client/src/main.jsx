4795c7273ef8aca69bb81cb0dbb2cb4cf37a34cb
prathamgupta22 <pratham.guptapg01@gmail.com>
2024-03-02 00:28:20 +0530

    f

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://blackcoffer-dashboard.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
